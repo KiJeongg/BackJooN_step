123a76290c5c2481d50b592b98738f3fad80cdcb
KiJeongg <rlwjd282@naver.com>
2025-04-08 22:23:07 +0900

    Add files via upload

---
+++ Main10.java
@@ -0,0 +1,15 @@
+//백준 10951 문제
+
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
