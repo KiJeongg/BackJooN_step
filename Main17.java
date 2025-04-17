a3267143b38d6d7cd2469fde4a05fefc39f74cfe
KiJeongg <rlwjd282@naver.com>
2025-04-17 23:46:38 +0900

    Add files via upload

---
+++ Main17.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+             String s = sc.next();
+             int[] arr = new int[26];
+             Arrays.fill(arr, -1);
+
+             for(int i=0; i<s.length(); i++) {
+                 char ch = s.charAt(i);
+                 int index = ch-'a';
+
+                if(arr[index]==-1) {
+                    arr[index] =i;
+                }
+             }
+
+             for(int i=0;i<26;i++) {
+                 System.out.print(arr[i]+ " ");
+             }
+
+    }
+}
